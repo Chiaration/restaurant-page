586e283c5c0e213086ef076da12914dc4827d328
Chiaration <45766347+Chiaration@users.noreply.github.com>
2021-10-28 19:46:24 +0100

    - Removed useless code - Confined code to Index.js - Tab system working

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import home from './pages/home.js';
 import menu from './pages/menu.js'
 import contact from './pages/contact.js'
-import button_function from './functions/tab_button_functions.js'
 import './style.css';
 
+// Creating TabList function
 const createTablist = (() => {
     // Grab the content div from HTML
     const content_div = document.querySelector('#content');
@@ -22,15 +22,15 @@ const createTablist = (() => {
     const home_button = document.createElement('Button');
     home_button.classList.add('tablinks');
     home_button.innerText = "Home"
-    home_button.onclick = button_function.button_function(event, "Home");
+    home_button.setAttribute('id', 'default');
 
     const menu_button = document.createElement('Button');
     menu_button.classList.add('tablinks');
-    menu_button.innerText = "Menu"
+    menu_button.innerText = "Menu";
 
     const contact_button = document.createElement('Button');
     contact_button.classList.add('tablinks');
-    contact_button.innerText = "Contact"
+    contact_button.innerText = "Contact";
 
     // Adding button to div
     tab_div.appendChild(home_button);
@@ -39,10 +39,45 @@ const createTablist = (() => {
 
     // Adding tab div to homepage
     content_div.appendChild(tab_div);
+
+    return {home_button, menu_button, contact_button}
 })();
 
+// Imported modules functions
 home.createHomepage();
 menu.createMenu();
 contact.createContact();
-button_function.button_function();
 
+// Button Functions
+function button_function(e){
+    console.log("BUTTON FUNCTION")
+
+    // Variable Declaration
+    let tabcontent, tablinks;
+
+    tabcontent = document.querySelectorAll('.tabcontent');
+    tabcontent.forEach(tab => {
+        tab.style.display = "none";
+    });
+
+    tablinks = document.querySelectorAll('.tablinks');
+    tablinks.forEach(tab => {
+        tab.className = tab.className.replace(' active', '');
+    })
+    
+    console.log(document.getElementById(e.currentTarget.innerText))
+    document.getElementById(e.currentTarget.innerText).style.display = 'block';
+    e.currentTarget.classname += " active";
+};
+
+createTablist.home_button.addEventListener("click", () => {
+    button_function(event)}
+);
+createTablist.menu_button.addEventListener("click", () => {
+    button_function(event)}
+);
+createTablist.contact_button.addEventListener("click", () => {
+    button_function(event)}
+);
+
+document.getElementById("default").click();
